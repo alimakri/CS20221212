ff03ef4b4b60a5e5cea72c06574e7ca3357e33b8
alimakri <github@makri.fr>
2022-12-13 16:18:13 +0100

    16:18

---
+++ DemoNullable/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Runtime.Serialization.Formatters.Soap;
+
+namespace DemoNullable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int? i = null;
+            List<Personne> p = new List<Personne> { new Personne { Nom = "Ali" }, new Personne { Nom = "Pierre" } };
+
+             // Serialization
+            IFormatter formatter1 = new SoapFormatter();
+            Stream stream1 = new FileStream("personnes.xml", FileMode.Create, FileAccess.Write, FileShare.None);
+            formatter1.Serialize(stream1, p);
+            stream1.Close();
+
+            // Déserialization
+            IFormatter formatter2 = new SoapFormatter();
+            Stream stream2 = new FileStream("personnes.xml", FileMode.Open, FileAccess.Read, FileShare.Read);
+            Personne obj = (Personne)formatter2.Deserialize(stream2);
+            stream2.Close();
+        }
+    }
+    [Serializable]
+    class Personne { public string Nom; }
+}
---
+++ Generique/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new ActiviteSportive<Foot>();
+            var balle=a.TypeBalle();
+            Console.WriteLine(balle);
+            Console.ReadLine();
+        }
+    }
+    class ActiviteSportive<T> where T : SportAvecBalle, new()
+    {
+        public string TypeBalle()
+        {
+            var f = new T();
+            return f.TypeBalle();
+        }
+    }
+    public class Foot : SportAvecBalle
+    {
+        public override string TypeBalle() { return "Ballon"; }
+    }
+    public class Rugby : SportAvecBalle
+    {
+        public override string TypeBalle() { return "Oval"; }
+    }
+    public class SportAvecBalle
+    {
+        public virtual string TypeBalle() { return "?"; }
+    }
+    public class Natation
+    {
+    }
+}

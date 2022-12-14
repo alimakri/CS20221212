bd1ab37e9a5a1d6666b2c55a2a484a9661326322
alimakri <github@makri.fr>
2022-12-14 15:48:06 +0100

    15:48

---
+++ DemoWpfApp/Bases/ViewModelBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+
+namespace Bigramme.Common
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            this.OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            var handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+        #endregion
+    }
+}
--- DemoWpfApp/ViewModels/MainWindowViewModel.cs
+++ DemoWpfApp/ViewModels/MainWindowViewModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
-using GalaSoft.MvvmLight.Command;
+using DemoWpfApp.Bases;
 
 namespace DemoWpfApp.ViewModels
 {
